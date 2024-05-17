a8a4f69fa5a382ac7494064da4719e9fdf754454
Parminder Singh <parmindersingh.swe@gmail.com>
2024-05-17 13:34:32 -0300

    Remove Element

---
+++ Problems.Library/Easy/RemoveElement/RemoveElement.cs
@@ -0,0 +1,18 @@
+﻿namespace Problems.Library.Easy.RemoveElement
+{
+    public class RemoveElement
+    {
+        public int Remove(int[] nums, int val)
+        {
+            int current = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != val)
+                {
+                    nums[current++] = nums[i];
+                }
+            }
+            return current;
+        }
+    }
+}
---
+++ Problems.Test/Easy/RemoveElement.cs
@@ -0,0 +1,27 @@
+﻿namespace Problems.Test.Easy
+{
+    public class RemoveElement
+    {
+        [Theory]
+        [InlineData(new int[] { 3, 2, 2, 3 }, 3, 2, new int[] { 2, 2 })]
+        [InlineData(new int[] { 0, 1, 2, 2, 3, 0, 4, 2 }, 2, 5, new int[] { 0, 1, 4, 0, 3 })]
+        public void RemoveElementTest(int[] nums, int val, int expectedK, int[] expectedNums)
+        {
+            // Arrange
+            Library.Easy.RemoveElement.RemoveElement solution = new Library.Easy.RemoveElement.RemoveElement();
+
+            // Act
+            int k = solution.Remove(nums, val);
+
+            // Assert
+            Assert.Equal(expectedK, k);
+            Array.Sort(nums, 0, k); // Sort the first k elements
+            Array.Sort(expectedNums);
+            for (int i = 0; i < k; i++)
+            {
+                Assert.Equal(expectedNums[i], nums[i]);
+            }
+        }
+
+    }
+}
