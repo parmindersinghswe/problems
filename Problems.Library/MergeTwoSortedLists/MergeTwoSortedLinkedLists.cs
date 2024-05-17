93694e7ef021f60321b0b375a30a75f98609dd3b
Parminder Singh <parmindersingh.swe@gmail.com>
2024-05-16 22:03:34 -0300

    MergeTwoSortedLinkedLists

---
+++ Problems.Library/MergeTwoSortedLists/MergeTwoSortedLinkedLists.cs
@@ -0,0 +1,37 @@
+﻿namespace Problems.Library.MergeTwoSortedLists
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    public class MergeTwoSortedLinkedLists
+    {
+        public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+        {
+            ListNode dummy = new ListNode();
+            ListNode current = dummy;
+            while (list1 != null && list2 != null)
+            {
+                if (list1.val < list2.val)
+                {
+                    current.next = list1;
+                    list1 = list1.next;
+                }
+                else
+                {
+                    current.next = list2;
+                    list2 = list2.next;
+                }
+                current = current.next;
+            }
+            current.next = list1 ?? list2;
+            return dummy.next;
+        }
+    }
+}
