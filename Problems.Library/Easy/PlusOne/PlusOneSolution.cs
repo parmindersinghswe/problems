087648ff6ecb1ee1a5ca06aefc4d77810578ca24
Parminder Singh <parmindersingh.swe@gmail.com>
2024-05-18 13:46:58 -0300

    Plus One

---
+++ Problems.Library/Easy/PlusOne/PlusOneSolution.cs
@@ -0,0 +1,21 @@
+﻿namespace Problems.Library.Easy.PlusOne
+{
+    public class PlusOneSolution
+    {
+        public int[] Increment(int[] digits)
+        {
+            for (int i = digits.Length - 1; i >= 0; i--)
+            {
+                if (digits[i] < 9)
+                {
+                    digits[i]++;
+                    return digits;
+                }
+                digits[i] = 0;
+            }
+            int[] newDigits = new int[digits.Length + 1];
+            newDigits[0] = 1;
+            return newDigits;
+        }
+    }
+}
---
+++ Problems.Test/Easy/PlusOneTests.cs
@@ -0,0 +1,26 @@
+﻿using Problems.Library.Easy.PlusOne;
+
+namespace Problems.Test.Easy
+{
+    public class PlusOneTests
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 2, 3 }, new int[] { 1, 2, 4 })]
+        [InlineData(new int[] { 4, 3, 2, 1 }, new int[] { 4, 3, 2, 2 })]
+        [InlineData(new int[] { 9 }, new int[] { 1, 0 })]
+        [InlineData(new int[] { 9, 9 }, new int[] { 1, 0, 0 })]
+        [InlineData(new int[] { 0 }, new int[] { 1 })]
+        [InlineData(new int[] { 9, 8, 9 }, new int[] { 9, 9, 0 })]
+        public void PlusOne_Tests(int[] input, int[] expected)
+        {
+            // Arrange
+            var solution = new PlusOneSolution();
+
+            // Act
+            int[] result = solution.Increment(input);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
