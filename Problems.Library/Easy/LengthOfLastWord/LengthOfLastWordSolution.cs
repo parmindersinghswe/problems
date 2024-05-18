a658f868a6711cede3e5cc2b105d816d0d5e052f
Parminder Singh <parmindersingh.swe@gmail.com>
2024-05-18 13:26:10 -0300

    Length Of Last Word

---
+++ Problems.Library/Easy/LengthOfLastWord/LengthOfLastWordSolution.cs
@@ -0,0 +1,22 @@
+﻿namespace Problems.Library.Easy.LengthOfLastWord
+{
+    public class LengthOfLastWordSolution
+    {
+        public int Length(string s)
+        {
+            int i = s.Length - 1;
+            int length = 0;
+            while (i >= 0 && s[i] == ' ')
+            {
+                i--;
+            }
+
+            while (i >= 0 && s[i] != ' ')
+            {
+                length++;
+                i--;
+            }
+            return length;
+        }
+    }
+}
---
+++ Problems.Test/Easy/LengthOfLastWordTests.cs
@@ -0,0 +1,25 @@
+﻿using Problems.Library.Easy.LengthOfLastWord;
+namespace Problems.Test.Easy
+{
+    public class LengthOfLastWordTests
+    {
+        [Theory]
+        [InlineData("Hello World", 5)]
+        [InlineData("   fly me   to   the moon  ", 4)]
+        [InlineData("luffy is still joyboy", 6)]
+        [InlineData("", 0)]
+        [InlineData("a", 1)]
+        [InlineData("a ", 1)]
+        public void LengthOfLastWord_ShouldReturnExpectedResult(string input, int expected)
+        {
+            // Arrange
+            var solution = new LengthOfLastWordSolution();
+
+            // Act
+            int result = solution.Length(input);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
