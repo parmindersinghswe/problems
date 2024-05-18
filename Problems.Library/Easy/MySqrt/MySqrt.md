20a07bf1c6724bda23ee0a8be7118db1e0b747e8
Parminder Singh <parmindersingh.swe@gmail.com>
2024-05-18 15:27:23 -0300

    My Sqrt

---
+++ Problems.Library/Easy/MySqrt/MySqrt.md
@@ -0,0 +1,20 @@
+# Square Root of x
+
+## Problem Description
+
+Given a non-negative integer `x`, the task is to return the square root of `x` rounded down to the nearest integer. The returned integer should be non-negative as well. You must not use any built-in exponent function or operator (e.g., `pow(x, 0.5)` in C++ or `x ** 0.5` in Python).
+
+## Examples
+
+### Example 1:
+- **Input:** `x = 4`
+- **Output:** `2`
+- **Explanation:** The square root of 4 is 2, so we return 2.
+
+### Example 2:
+- **Input:** `x = 8`
+- **Output:** `2`
+- **Explanation:** The square root of 8 is approximately 2.82842, and since we round it down to the nearest integer, 2 is returned.
+
+## Constraints
+- \(0 \leq x \leq 2^{31} - 1\)
---
+++ Problems.Test/Easy/MySqrtTests.cs
@@ -0,0 +1,26 @@
+﻿using Problems.Library.Easy.MySqrt;
+namespace Problems.Test.Easy
+{
+    public class MySqrtTests
+    {
+        [Theory]
+        [InlineData(4, 2)]    
+        [InlineData(8, 2)]    
+        [InlineData(0, 0)]    
+        [InlineData(1, 1)]    
+        [InlineData(15, 3)]   
+        [InlineData(16, 4)]   
+        [InlineData(25, 5)]   
+        [InlineData(27, 5)]   
+        [InlineData(100, 10)] 
+        public void MySqrt_Tests(int input, int expectedOutput)
+        {
+            var solution = new MySqrtSolution();
+            // Act
+            int actualOutput = solution.Find(input);
+
+            // Assert
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+    }
+}
