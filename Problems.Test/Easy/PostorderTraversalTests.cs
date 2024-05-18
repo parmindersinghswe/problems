bf1ef8bb6597ab89a85a3b6d4c752d0a3c8bb0ff
Parminder Singh <parmindersingh.swe@gmail.com>
2024-05-18 17:33:09 -0300

    Binary Tree Postorder Traverse

---
+++ Problems.Test/Easy/PostorderTraversalTests.cs
@@ -0,0 +1,35 @@
+﻿using Problems.Test.Services;
+using Problems.Library.Models;
+using Problems.Library.Easy.BinaryTreePostorderTraversal;
+
+namespace Problems.Test.Easy
+{
+    public class PostorderTraversalTests
+    {
+        public static IEnumerable<object[]> PostorderTraversalTestData()
+        {
+            yield return new object[] { new int?[] { 1, null, 2, 3 }, new int[] { 3, 2, 1 } };
+            yield return new object[] { new int?[] { }, new int[] { } };
+            yield return new object[] { new int?[] { 1 }, new int[] { 1 } };
+            yield return new object[] { new int?[] { 1, 2, 3, 4, 5, null, 6 }, new int[] { 4, 5, 2, 6, 3, 1 } };
+            yield return new object[] { new int?[] { 1, 2, 3, null, 4, null, 5 }, new int[] { 4, 2, 5, 3, 1 } };
+            yield return new object[] { new int?[] { 1, null, 2, null, 3, null, 4 }, new int[] { 4, 3, 2, 1 } };
+            yield return new object[] { new int?[] { 3, 9, 20, null, null, 15, 7 }, new int[] { 9, 15, 7, 20, 3 } };
+            yield return new object[] { new int?[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 }, new int[] { 8, 9, 4, 10, 11, 5, 2, 12, 13, 6, 14, 15, 7, 3, 1 } };
+        }
+
+        [Theory]
+        [MemberData(nameof(PostorderTraversalTestData))]
+        public void PostorderTraversal_Tests(int?[] treeValues, int[] expectedOutput)
+        {
+            // Arrange
+            TreeNode? root = TreeService.CreateTree(treeValues);
+            BinaryTreePostorderTraversalSolution solution = new BinaryTreePostorderTraversalSolution();
+            // Act
+            IList<int> result = solution.PostorderTraversal(root);
+
+            // Assert
+            Assert.Equal(expectedOutput, result);
+        }
+    }
+}
---
+++ Problems.Test/Services/TreeService.cs
@@ -0,0 +1,38 @@
+﻿using Problems.Library.Models;
+
+namespace Problems.Test.Services
+{
+    public static class TreeService
+    {
+        public static TreeNode? CreateTree(int?[] values)
+        {
+            if (values.Length == 0 || values[0] == null) return null;
+
+            TreeNode root = new TreeNode(values[0]!.Value);
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            int i = 1;
+
+            while (queue.Count > 0 && i < values.Length)
+            {
+                TreeNode current = queue.Dequeue();
+
+                if (i < values.Length && values[i] != null)
+                {
+                    current.left = new TreeNode(values[i]!.Value);
+                    queue.Enqueue(current.left);
+                }
+                i++;
+
+                if (i < values.Length && values[i] != null)
+                {
+                    current.right = new TreeNode(values[i]!.Value);
+                    queue.Enqueue(current.right);
+                }
+                i++;
+            }
+
+            return root;
+        }
+    }
+}
