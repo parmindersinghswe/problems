8b0dc1dedd63604f14fe8de7a11d8a064b430f64
Parminder Singh <parmindersingh.swe@gmail.com>
2024-05-19 18:12:09 -0300

    Valid Palindrome

---
+++ Problems.Library/Easy/Palidrome/NumberPalindrome/ValidPalindromeSolution.cs
@@ -0,0 +1,58 @@
+﻿namespace Problems.Library.Easy.Palidrome.NumberPalindrome
+{
+    /// <summary>
+    /// Checks if given input is Valid Palindrome or not.
+    /// </summary>
+    public class ValidPalindromeSolution
+    {
+        /// <summary>
+        /// Helper method to convert lower case letter to upper case letter
+        /// </summary>
+        /// <param name="c">Input character to convert to upper case.</param>
+        /// <returns></returns>
+        private char Upper(char c)
+        {
+            if (c >= 97 && c <= 122)
+            {
+                return (char)(c - 32);
+            }
+            return c;
+        }
+        /// <summary>
+        /// Return true if given input string is valid palindrome otherwise false after removing all the non-alphanumeric characters and ignoring the character case.
+        /// </summary>
+        /// <param name="s">Input string value.</param>
+        /// <returns></returns>
+        public bool IsPalindrome(string s)
+        {
+            if (s == null || s == "")
+            {
+                return true;
+            }
+            int start = 0;
+            int end = s.Length - 1;
+            while (start < end)
+            {
+                char startChar = Upper(s[start]);
+                char endChar = Upper(s[end]);
+                if ((startChar < 65 || startChar > 90) && !(startChar >= 48 && startChar <= 57))
+                {
+                    start++;
+                    continue;
+                }
+                if ((endChar < 65 || endChar > 90) && !(endChar >= 48 && endChar <= 57))
+                {
+                    end--;
+                    continue;
+                }
+                if (startChar != endChar)
+                {
+                    return false;
+                }
+                start++;
+                end--;
+            }
+            return true;
+        }
+    }
+}
---
+++ Problems.Test/Easy/ValidPalindromeTests.cs
@@ -0,0 +1,28 @@
+﻿
+using Problems.Library.Easy.Palidrome.NumberPalindrome;
+
+namespace Problems.Test.Easy
+{
+    public class ValidPalindromeTests
+    {
+        public static IEnumerable<object[]> IsPalindromeTestData()
+        {
+            yield return new object[] { "A man, a plan, a canal: Panama", true };
+            yield return new object[] { "race a car", false };
+            yield return new object[] { " ", true };
+            yield return new object[] { "No lemon, no melon", true };
+            yield return new object[] { "Was it a car or a cat I saw?", true };
+            yield return new object[] { "Able was I, I saw Elba", true };
+            yield return new object[] { "0P", false };
+        }
+
+        [Theory]
+        [MemberData(nameof(IsPalindromeTestData))]
+        public void ValidPalindrome_Tests(string s, bool expected)
+        {
+            ValidPalindromeSolution solution = new ValidPalindromeSolution();
+            bool result = solution.IsPalindrome(s);
+            Assert.Equal(expected, result);
+        }
+    }
+}
