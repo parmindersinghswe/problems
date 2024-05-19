ac5d8ee940162d58810e3f3e5cb6b8c4d2966718
Parminder Singh <parmindersingh.swe@gmail.com>
2024-05-18 23:25:23 -0300

    Binary Tree Preorder Traversal

---
+++ Problems.Library/Easy/BinaryTreeTraversal/BinaryTreeTraversalSolution.cs
@@ -0,0 +1,50 @@
+﻿using Problems.Library.Models;
+
+namespace Problems.Library.Easy.BinaryTreeTraversal
+{
+    public class BinaryTreeTraversalSolution
+    {
+        public IList<int> PostorderTraversal(TreeNode? root)
+        {
+            IList<int> result = new List<int>();
+            if (root != null)
+            {
+                TraversePostOrder(root, result);
+            }
+            return result;
+        }
+        public IList<int> PreorderTraversal(TreeNode? root)
+        {
+            IList<int> list = new List<int>();
+            if (root != null)
+            {
+                TraversePreOrder(root, list);
+            }
+            return list;
+        }
+        private void TraversePreOrder(TreeNode root, IList<int> list)
+        {
+            list.Add(root.val);
+            if (root.left != null)
+            {
+                TraversePreOrder(root.left, list);
+            }
+            if (root.right != null)
+            {
+                TraversePreOrder(root.right, list);
+            }
+        }
+        private void TraversePostOrder(TreeNode root, IList<int> list)
+        {
+            if (root.left != null)
+            {
+                TraversePostOrder(root.left, list);
+            }
+            if (root.right != null)
+            {
+                TraversePostOrder(root.right, list);
+            }
+            list.Add(root.val);
+        }
+    }
+}
---
+++ Problems.Test/Easy/BinaryTreeTreversalTests.cs
@@ -0,0 +1,60 @@
+﻿using Problems.Test.Services;
+using Problems.Library.Models;
+using Problems.Library.Easy.BinaryTreeTraversal;
+
+namespace Problems.Test.Easy
+{
+    public class BinaryTreeTreversalTests
+    {
+        public static IEnumerable<object[]> PostorderTraversalTestData()
+        {
+            yield return new object[] { new int?[] { 1, null, 2, 3 }, new int[] { 3, 2, 1 } };
+            yield return new object[] { new int?[] { }, new int[] { } };
+            yield return new object[] { new int?[] { 1 }, new int[] { 1 } };
+            yield return new object[] { new int?[] { 1, 2, 3, 4, 5, null, 6 }, new int[] { 4, 5, 2, 6, 3, 1 } };
+            yield return new object[] { new int?[] { 1, 2, 3, null, 4, null, 5 }, new int[] { 4, 2, 5, 3, 1 } };
+            yield return new object[] { new int?[] { 1, null, 2, null, 3, null, 4 }, new int[] { 4, 3, 2, 1 } };
+            yield return new object[] { new int?[] { 3, 9, 20, null, null, 15, 7 }, new int[] { 9, 15, 7, 20, 3 } };
+            yield return new object[] { new int?[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 }, new int[] { 8, 9, 4, 10, 11, 5, 2, 12, 13, 6, 14, 15, 7, 3, 1 } };
+        }
+
+        [Theory]
+        [MemberData(nameof(PostorderTraversalTestData))]
+        public void PostorderTraversal_Tests(int?[] treeValues, int[] expectedOutput)
+        {
+            // Arrange
+            TreeNode? root = TreeService.CreateTree(treeValues);
+            BinaryTreeTraversalSolution solution = new BinaryTreeTraversalSolution();
+            // Act
+            IList<int> result = solution.PostorderTraversal(root);
+
+            // Assert
+            Assert.Equal(expectedOutput, result);
+        }
+        public static IEnumerable<object[]> PreorderTraversalTestData()
+        {
+            yield return new object[] { new int?[] { 1, null, 2, 3 }, new int[] { 1, 2, 3 } };
+            yield return new object[] { new int?[] { }, new int[] { } };
+            yield return new object[] { new int?[] { 1 }, new int[] { 1 } };
+            yield return new object[] { new int?[] { 1, 2, 3, 4, 5, null, 6 }, new int[] { 1, 2, 4, 5, 3, 6 } };
+            yield return new object[] { new int?[] { 1, 2, 3, null, 4, null, 5 }, new int[] { 1, 2, 4, 3, 5 } };
+            yield return new object[] { new int?[] { 1, null, 2, null, 3, null, 4 }, new int[] { 1, 2, 3, 4 } };
+            yield return new object[] { new int?[] { 3, 9, 20, null, null, 15, 7 }, new int[] { 3, 9, 20, 15, 7 } };
+            yield return new object[] { new int?[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 }, new int[] { 1, 2, 4, 8, 9, 5, 10, 11, 3, 6, 12, 13, 7, 14, 15 } };
+        }
+
+        [Theory]
+        [MemberData(nameof(PreorderTraversalTestData))]
+        public void Test_PreorderTraversal(int?[] treeValues, int[] expectedOutput)
+        {
+            // Arrange
+            TreeNode? root = TreeService.CreateTree(treeValues);
+            BinaryTreeTraversalSolution solution = new BinaryTreeTraversalSolution();
+            // Act
+            IList<int> result = solution.PreorderTraversal(root);
+
+            // Assert
+            Assert.Equal(expectedOutput, result);
+        }
+    }
+}
