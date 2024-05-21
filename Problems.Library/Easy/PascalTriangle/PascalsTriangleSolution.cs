e883d8cf3766c772654d887ab204a28deeefd9aa
Parminder Singh <parmindersingh.swe@gmail.com>
2024-05-20 21:30:29 -0300

    Pascals Triangle

---
+++ Problems.Library/Easy/PascalTriangle/PascalsTriangleSolution.cs
@@ -0,0 +1,29 @@
+﻿namespace Problems.Library.Easy.PascalTriangle
+{
+    public class PascalsTriangleSolution
+    {
+        public IList<IList<int>> Generate(int numRows)
+        {
+            IList<IList<int>> list = new List<IList<int>>();
+            if (numRows == 0) return list;
+
+            list.Add(new List<int> { 1 });
+
+            for (int i = 1; i < numRows; i++)
+            {
+                IList<int> row = new List<int> { 1 };
+                IList<int> prevRow = list[i - 1];
+
+                for (int j = 1; j < i; j++)
+                {
+                    row.Add(prevRow[j - 1] + prevRow[j]);
+                }
+
+                row.Add(1);
+                list.Add(row);
+            }
+
+            return list;
+        }
+    }
+}
---
+++ Problems.Test/Easy/PascalsTriangleTests.cs
@@ -0,0 +1,26 @@
+﻿using Problems.Library.Easy.PascalTriangle;
+
+namespace Problems.Test.Easy
+{
+    public class PascalsTriangleTests
+    {
+        public static IEnumerable<object[]> GenerateTestData()
+        {
+            yield return new object[] { 0, new List<IList<int>>() };
+            yield return new object[] { 1, new List<IList<int>> { new List<int> { 1 } } };
+            yield return new object[] { 2, new List<IList<int>> { new List<int> { 1 }, new List<int> { 1, 1 } } };
+            yield return new object[] { 3, new List<IList<int>> { new List<int> { 1 }, new List<int> { 1, 1 }, new List<int> { 1, 2, 1 } } };
+            yield return new object[] { 4, new List<IList<int>> { new List<int> { 1 }, new List<int> { 1, 1 }, new List<int> { 1, 2, 1 }, new List<int> { 1, 3, 3, 1 } } };
+            yield return new object[] { 5, new List<IList<int>> { new List<int> { 1 }, new List<int> { 1, 1 }, new List<int> { 1, 2, 1 }, new List<int> { 1, 3, 3, 1 }, new List<int> { 1, 4, 6, 4, 1 } } };
+        }
+
+        [Theory]
+        [MemberData(nameof(GenerateTestData))]
+        public void PascalsTriangle_Tests(int numRows, IList<IList<int>> expected)
+        {
+            PascalsTriangleSolution solution = new PascalsTriangleSolution();
+            IList<IList<int>> result = solution.Generate(numRows);
+            Assert.Equal(expected, result);
+        }
+    }
+}
