727310b79865673022d329d9c94107548441cadd
Parminder Singh <parmindersingh.swe@gmail.com>
2024-05-20 22:48:43 -0300

    Path Sum

---
+++ Problems.Library/Easy/PathSum/PathSumSolution.cs
@@ -0,0 +1,27 @@
+﻿using Problems.Library.Models;
+
+namespace Problems.Library.Easy.PathSum
+{
+    /// <summary>
+    /// Solutions of the Path Sum of the tree
+    /// </summary>
+    public class PathSumSolution
+    {
+        /// <summary>
+        /// Return true if any path of the tree nodes sum is equal to the target Sum.
+        /// </summary>
+        /// <param name="root">Input root node of the binary tree.</param>
+        /// <param name="targetSum">Input target sum.</param>
+        /// <returns></returns>
+        public bool HasPathSum(TreeNode? root, int targetSum)
+        {
+            if (root == null) return false;
+            if (root.left == null && root.right == null)
+            {
+                return root.val == targetSum;
+            }
+            return HasPathSum(root.left, targetSum - root.val) ||
+                   HasPathSum(root.right, targetSum - root.val);
+        }
+    }
+}
---
+++ Problems.Test/Easy/PathSumTests.cs
@@ -0,0 +1,28 @@
+﻿using Problems.Test.Services;
+using Problems.Library.Models;
+using Problems.Library.Easy.PathSum;
+
+namespace Problems.Test.Easy
+{
+    public class PathSumTests
+    {
+        public static IEnumerable<object[]> HasPathSumTestData()
+        {
+            yield return new object[] { new int?[] { 5, 4, 8, 11, null, 13, 4, 7, 2, null, null, null, 1 }, 22, true };
+            yield return new object[] { new int?[] { 1, 2, 3 }, 5, false };
+            yield return new object[] { new int?[] { 1, 2 }, 0, false };
+            yield return new object[] { new int?[] { }, 0, false };
+            yield return new object[] { new int?[] { 1, 2, null, 3, null, 4, null, 5 }, 15, true };
+        }
+
+        [Theory]
+        [MemberData(nameof(HasPathSumTestData))]
+        public void HasPathSum_Tests(int?[] treeValues, int targetSum, bool expected)
+        {
+            TreeNode root = TreeService.CreateTree(treeValues)!;
+            PathSumSolution solution = new PathSumSolution();
+            bool result = solution.HasPathSum(root, targetSum);
+            Assert.Equal(expected, result);
+        }
+    }
+}
