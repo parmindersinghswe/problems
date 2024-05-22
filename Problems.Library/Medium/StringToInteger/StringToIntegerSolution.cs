453ab60d17467e925e9b5f9b39c92d87c15d7f78
Parminder Singh <parmindersingh.swe@gmail.com>
2024-05-22 20:17:13 -0300

    Integer To String

---
+++ Problems.Library/Medium/StringToInteger/StringToIntegerSolution.cs
@@ -0,0 +1,60 @@
+﻿namespace Problems.Library.Medium.StringToInteger
+{
+    /// <summary>
+    /// Convert the String To Integers
+    /// </summary>
+    public class StringToIntegerSolution
+    {
+        /// <summary>
+        /// return the first integer value from the string ignoring any leading spaces and considering an optional leading sign character.
+        /// </summary>
+        /// <param name="s">Input String Value</param>
+        /// <returns></returns>
+        public int MyAtoi(string s)
+        {
+            int i = 0;
+            int returnValue = 0;
+            int totalChars = s.Length;
+            int sign = 1;
+            while (i < totalChars && s[i] == ' ')
+            {
+                i++;
+            }
+            if (i < totalChars)
+            {
+                if (s[i] == '-')
+                {
+                    sign = -1;
+                    i++;
+                }
+                else if (s[i] == '+')
+                {
+                    i++;
+                }
+                while (i < totalChars && s[i] >= 48 && s[i] <= 57)
+                {
+                    if (returnValue == 0)
+                    {
+                        returnValue = CharToInt(s[i]);
+                    }
+                    else
+                    {
+                        int newDigit = CharToInt(s[i]);
+                        if (returnValue > (Int32.MaxValue - newDigit) / 10)
+                        {
+                            return sign == 1 ? Int32.MaxValue : Int32.MinValue;
+                        }
+                        returnValue = (returnValue * 10) + newDigit;
+                    }
+                    i++;
+                }
+            }
+            returnValue *= sign;
+            return returnValue;
+        }
+        private int CharToInt(char ch)
+        {
+            return ch - 48;
+        }
+    }
+}
---
+++ Problems.Test/Medium/StringToIntegerTests.cs
@@ -0,0 +1,42 @@
+﻿using Problems.Library.Medium.StringToInteger;
+
+namespace Problems.Test.Medium
+{
+    public class StringToIntegerTests
+    {
+        public static IEnumerable<object[]> MyAtoiTestData()
+        {
+            yield return new object[] { "42", 42 };
+            yield return new object[] { "   -42", -42 };
+            yield return new object[] { "4193 with words", 4193 };
+            yield return new object[] { "words and 987", 0 };
+            yield return new object[] { "-91283472332", Int32.MinValue }; 
+            yield return new object[] { "2147483648", Int32.MaxValue };
+            yield return new object[] { "+1", 1 };
+            yield return new object[] { "   +0 123", 0 };
+            yield return new object[] { "00000-42a1234", 0 };
+            yield return new object[] { "-5-", -5 };
+            yield return new object[] { "   0000000000012345678", 12345678 };
+            yield return new object[] { "   +123456789012345", 2147483647 }; 
+            yield return new object[] { "   -123456789012345", -2147483648 }; 
+            yield return new object[] { "   +0", 0 }; 
+            yield return new object[] { "   -0", 0 }; 
+            yield return new object[] { "2147483647", 2147483647 }; 
+            yield return new object[] { "-2147483648", -2147483648 }; 
+            yield return new object[] { "2147483646", 2147483646 }; 
+            yield return new object[] { "-2147483647", -2147483647 }; 
+            yield return new object[] { "   2147483648", 2147483647 }; 
+            yield return new object[] { "   -2147483649", -2147483648 }; 
+            yield return new object[] { "   + 413", 0 }; 
+        }
+
+        [Theory]
+        [MemberData(nameof(MyAtoiTestData))]
+        public void Test_MyAtoi(string s, int expected)
+        {
+            StringToIntegerSolution solution = new StringToIntegerSolution();
+            int result = solution.MyAtoi(s);
+            Assert.Equal(expected, result);
+        }
+    }
+}
