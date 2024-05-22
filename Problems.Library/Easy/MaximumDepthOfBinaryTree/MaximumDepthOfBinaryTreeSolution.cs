aae414857a22a3e5673de1d4abd70506f845b13a
Parminder Singh <parmindersingh.swe@gmail.com>
2024-05-22 20:36:56 -0300

    Maximum Depth Of Binary Tree

---
+++ Problems.Library/Easy/MaximumDepthOfBinaryTree/MaximumDepthOfBinaryTreeSolution.cs
@@ -0,0 +1,14 @@
+﻿using Problems.Library.Models;
+namespace Problems.Library.Easy.MaximumDepthOfBinaryTree
+{
+    public class MaximumDepthOfBinaryTreeSolution
+    {
+        public int MaxDepth(TreeNode? root)
+        {
+            if (root == null) return 0;
+            int leftDepth = MaxDepth(root.left);
+            int rightDepth = MaxDepth(root.right);
+            return (leftDepth > rightDepth ? leftDepth : rightDepth) + 1;
+        }
+    }
+}
---
+++ Problems.Test/Easy/MaximumDepthOfBinaryTreeTests.cs
@@ -0,0 +1,29 @@
+﻿using Problems.Library.Easy.MaximumDepthOfBinaryTree;
+using Problems.Library.Models;
+using Problems.Test.Services;
+
+namespace Problems.Test.Easy
+{
+    public class MaximumDepthOfBinaryTreeTests
+    {
+        public static IEnumerable<object[]> MaxDepthTestData()
+        {
+            yield return new object[] { new int?[] { 3, 9, 20, null, null, 15, 7 }, 3 };
+            yield return new object[] { new int?[] { 1, null, 2 }, 2 };
+            yield return new object[] { new int?[] { }, 0 }; 
+            yield return new object[] { new int?[] { 1, 2, 3, 4, null, null, 5 }, 3 };
+            yield return new object[] { new int?[] { 1 }, 1 }; 
+            yield return new object[] { new int?[] { 1, 2, 2, 3, null, null, 3, 4, null, null, 4 }, 4 };
+        }
+
+        [Theory]
+        [MemberData(nameof(MaxDepthTestData))]
+        public void Test_MaxDepth(int?[] treeValues, int expected)
+        {
+            TreeNode? root = TreeService.CreateTree(treeValues);
+            MaximumDepthOfBinaryTreeSolution solution = new MaximumDepthOfBinaryTreeSolution();
+            int result = solution.MaxDepth(root);
+            Assert.Equal(expected, result);
+        }
+    }
+}
