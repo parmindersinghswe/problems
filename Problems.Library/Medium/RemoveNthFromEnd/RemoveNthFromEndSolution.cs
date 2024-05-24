08ee1f6a242a4e9cb16bc633ae13e9776d5b4746
Parminder Singh <parmindersingh.swe@gmail.com>
2024-05-23 22:07:14 -0300

    Remove Nth From End

---
+++ Problems.Library/Medium/RemoveNthFromEnd/RemoveNthFromEndSolution.cs
@@ -0,0 +1,40 @@
+﻿using Problems.Library.Models;
+
+namespace Problems.Library.Medium.RemoveNthFromEnd
+{
+    /// <summary>
+    /// Solution to remove the N-th node from the end of a linked list.
+    /// </summary>
+    public class RemoveNthFromEndSolution
+    {
+        /// <summary>
+        /// Removes the N-th node from the end of the list and returns the head of the modified list.
+        /// </summary>
+        /// <param name="head">The head of the linked list.</param>
+        /// <param name="n">The position from the end of the list of the node to remove.</param>
+        /// <returns>The head of the modified linked list.</returns>
+        public ListNode? RemoveNthFromEnd(ListNode? head, int n)
+        {
+            if (head == null || n <= 0) return head;
+            ListNode dummy = new ListNode(0, head);
+            ListNode? first = dummy;
+            ListNode? second = dummy;
+            for (int i = 0; i <= n; i++)
+            {
+                if (first == null) return head;
+                first = first.next;
+            }
+            while (first != null)
+            {
+                first = first.next;
+                second = second!.next;
+            }
+
+            if (second?.next != null)
+            {
+                second.next = second.next.next;
+            }
+            return dummy.next;
+        }
+    }
+}
---
+++ Problems.Test/Medium/RemoveNthFromEndTests.cs
@@ -0,0 +1,36 @@
+﻿using Problems.Test.Services;
+using Problems.Library.Models;
+using Problems.Library.Medium.RemoveNthFromEnd;
+
+namespace Problems.Test.Medium
+{
+    public class RemoveNthFromEndTests
+    {
+        public static IEnumerable<object[]> RemoveNthFromEndTestData()
+        {
+            yield return new object[] { new int[] { 1, 2, 3, 4, 5 }, 2, new int[] { 1, 2, 3, 5 } };
+            yield return new object[] { new int[] { 1 }, 1, new int[] { } };
+            yield return new object[] { new int[] { 1, 2 }, 1, new int[] { 1 } };
+            yield return new object[] { new int[] { 1, 2 }, 2, new int[] { 2 } };
+            yield return new object[] { new int[] { 1, 2, 3 }, 3, new int[] { 2, 3 } };
+            yield return new object[] { new int[] { }, 1, new int[] { } };
+            yield return new object[] { new int[] { 1 }, 2, new int[] { 1 } };
+            yield return new object[] { new int[] { 1, 2, 3 }, 4, new int[] { 1, 2, 3 } };
+            yield return new object[] { new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, 10, new int[] { 2, 3, 4, 5, 6, 7, 8, 9, 10 } };
+            yield return new object[] { new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, 1, new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 } };
+            yield return new object[] { new int[] { 1, 2, 3, 4 }, 1, new int[] { 1, 2, 3 } };
+            yield return new object[] { new int[] { 1, 2, 3, 4 }, 4, new int[] { 2, 3, 4 } };
+        }
+
+        [Theory]
+        [MemberData(nameof(RemoveNthFromEndTestData))]
+        public void Test_RemoveNthFromEnd(int[] values, int n, int[] expected)
+        {
+            var solution = new RemoveNthFromEndSolution();
+            ListNode? head = LinkedListService.CreateLinkedList(values);
+            head = solution.RemoveNthFromEnd(head, n);
+            List<int> result = LinkedListService.ConvertToList(head);
+            Assert.Equal(expected, result);
+        }
+    }
+}
