3bc154d772aa504b0c88421646ca893a74084230
Parminder Singh <parmindersingh.swe@gmail.com>
2024-05-25 20:13:53 -0300

    Letter Combinations Of A Phone Number

---
+++ Problems.Library/Medium/LetterCombinationsOfAPhoneNumber/LetterCombinationsOfAPhoneNumberSolution.cs
@@ -0,0 +1,67 @@
+﻿using System.Text;
+
+namespace Problems.Library.Medium.LetterCombinationsOfAPhoneNumber
+{
+    /// <summary>
+    /// Solution to generate all possible letter combinations from a phone number.
+    /// </summary>
+    public class LetterCombinationsOfAPhoneNumberSolution
+    {
+        private readonly Dictionary<char, string> digitsToCharacters = new Dictionary<char, string>
+        {
+            { '2', "abc" },
+            { '3', "def" },
+            { '4', "ghi" },
+            { '5', "jkl" },
+            { '6', "mno" },
+            { '7', "pqrs" },
+            { '8', "tuv" },
+            { '9', "wxyz" }
+        };
+
+        /// <summary>
+        /// Generates all possible letter combinations that the number could represent.
+        /// </summary>
+        /// <param name="digits">A string containing digits from 2-9.</param>
+        /// <returns>A list of all possible letter combinations.</returns>
+        public IList<string> LetterCombinations(string digits)
+        {
+            IList<string> result = new List<string>();
+            if (!string.IsNullOrEmpty(digits))
+            {
+                StringBuilder currentCombination = new StringBuilder();
+                GenerateCombinations(currentCombination, result, digits, 0);
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Recursively generates all possible letter combinations.
+        /// </summary>
+        /// <param name="currentCombination">The current combination being built.</param>
+        /// <param name="result">The list to store the generated combinations.</param>
+        /// <param name="digits">The input string of digits.</param>
+        /// <param name="index">The current index in the digits string.</param>
+        private void GenerateCombinations(StringBuilder currentCombination, IList<string> result, string digits, int index)
+        {
+            if (index >= digits.Length)
+            {
+                result.Add(currentCombination.ToString());
+                return;
+            }
+
+            char currentDigit = digits[index];
+            if (digitsToCharacters.ContainsKey(currentDigit))
+            {
+                string letters = digitsToCharacters[currentDigit];
+                foreach (char letter in letters)
+                {
+                    currentCombination.Append(letter);
+                    GenerateCombinations(currentCombination, result, digits, index + 1);
+                    currentCombination.Length--;
+                }
+            }
+        }
+    }
+}
---
+++ Problems.Test/Medium/LetterCombinationsOfAPhoneNumberTests.cs
@@ -0,0 +1,31 @@
+﻿using Problems.Library.Medium.LetterCombinationsOfAPhoneNumber;
+
+namespace Problems.Test.Medium
+{
+    public class LetterCombinationsOfAPhoneNumberTests
+    {
+        public static IEnumerable<object[]> LetterCombinationsTestData()
+        {
+            yield return new object[] { "23", new List<string> { "ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf" } };
+            yield return new object[] { "", new List<string> { } };
+            yield return new object[] { "2", new List<string> { "a", "b", "c" } };
+            yield return new object[] { "7", new List<string> { "p", "q", "r", "s" } };
+            yield return new object[] { "78", new List<string> { "pt", "pu", "pv", "qt", "qu", "qv", "rt", "ru", "rv", "st", "su", "sv" } };
+            yield return new object[] { "234", new List<string> { "adg", "adh", "adi", "aeg", "aeh", "aei", "afg", "afh", "afi", "bdg", "bdh", "bdi", "beg", "beh", "bei", "bfg", "bfh", "bfi", "cdg", "cdh", "cdi", "ceg", "ceh", "cei", "cfg", "cfh", "cfi" } };
+            yield return new object[] { "9", new List<string> { "w", "x", "y", "z" } };
+            yield return new object[] { "79", new List<string> { "pw", "px", "py", "pz", "qw", "qx", "qy", "qz", "rw", "rx", "ry", "rz", "sw", "sx", "sy", "sz" } };
+            yield return new object[] { "56", new List<string> { "jm", "jn", "jo", "km", "kn", "ko", "lm", "ln", "lo" } };
+            yield return new object[] { "8", new List<string> { "t", "u", "v" } };
+            yield return new object[] { "345", new List<string> { "dgj", "dgk", "dgl", "dhj", "dhk", "dhl", "dij", "dik", "dil", "egj", "egk", "egl", "ehj", "ehk", "ehl", "eij", "eik", "eil", "fgj", "fgk", "fgl", "fhj", "fhk", "fhl", "fij", "fik", "fil" } };
+        }
+
+        [Theory]
+        [MemberData(nameof(LetterCombinationsTestData))]
+        public void Test_LetterCombinations(string digits, List<string> expected)
+        {
+            var solution = new LetterCombinationsOfAPhoneNumberSolution();
+            IList<string> result = solution.LetterCombinations(digits);
+            Assert.Equal(expected, result);
+        }
+    }
+}
