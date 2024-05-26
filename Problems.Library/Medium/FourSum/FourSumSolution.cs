4a06f25ff5b5ff3316284ea843b4e1da0c2554bd
Parminder Singh <parmindersingh.swe@gmail.com>
2024-05-26 13:20:44 -0300

    Four sum

---
+++ Problems.Library/Medium/FourSum/FourSumSolution.cs
@@ -0,0 +1,67 @@
+﻿namespace Problems.Library.Medium.FourSum
+{
+    /// <summary>
+    /// Solution to find all unique quadruplets in the array which give the sum of the target value.
+    /// </summary>
+    public class FourSumSolution
+    {
+        /// <summary>
+        /// Finds all unique quadruplets in the array which gives the sum of the target value.
+        /// </summary>
+        /// <param name="nums">The input array of integers.</param>
+        /// <param name="target">The target sum value.</param>
+        /// <returns>A list of lists, where each list contains four integers that sum up to the target value.</returns>
+        public IList<IList<int>> FourSum(int[] nums, int target)
+        {
+            Array.Sort(nums);
+            int len = nums.Length;
+            long sum;
+            int left;
+            int right;
+            IList<IList<int>> result = new List<IList<int>>();
+            HashSet<string> seen = new HashSet<string>();
+            for (int i = 0; i < len - 3; i++)
+            {
+                if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+                for (int j = i + 1; j < len - 2; j++)
+                {
+                    if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+
+                    left = j + 1;
+                    right = len - 1;
+
+                    while (left < right)
+                    {
+                        sum = (long)nums[i] + (long)nums[j] + (long)nums[left] + (long)nums[right];
+
+                        if (sum == target)
+                        {
+                            var quad = new List<int> { nums[i], nums[j], nums[left], nums[right] }.OrderBy(x=> x).ToList();
+
+                            string key = string.Join(",", quad);
+                            if (!seen.Contains(key))
+                            {
+                                result.Add(quad);
+                                seen.Add(key);
+                            }
+                            while (left < right && nums[left] == nums[left + 1]) left++;
+                            while (left < right && nums[right] == nums[right - 1]) right--;
+                            left++;
+                            right--;
+                        }
+                        else if (sum < target)
+                        {
+                            left++;
+                        }
+                        else
+                        {
+                            right--;
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Problems.Test/Medium/FourSumTests.cs
@@ -0,0 +1,52 @@
+﻿using Problems.Library.Medium.FourSum;
+
+namespace Problems.Test.Medium
+{
+    public class FourSumTests
+    {
+        public static IEnumerable<object[]> FourSumTestData()
+        {
+            yield return new object[] { new int[] { 1, 0, -1, 0, -2, 2 }, 0, new List<IList<int>> {
+            new List<int> { -2, -1, 1, 2 },
+            new List<int> { -2, 0, 0, 2 },
+            new List<int> { -1, 0, 0, 1 }
+        }};
+            yield return new object[] { new int[] { 2, 2, 2, 2, 2 }, 8, new List<IList<int>> {
+            new List<int> { 2, 2, 2, 2 }
+        }};
+            yield return new object[] { new int[] { -1, 0, 1, 2, -1, -4 }, -1, new List<IList<int>> {
+            new List<int> { -4, 0, 1, 2 },
+            new List<int> { -1, -1, 0, 1 }
+        }};
+            yield return new object[] { new int[] { 0, 0, 0, 0 }, 0, new List<IList<int>> {
+            new List<int> { 0, 0, 0, 0 }
+        }};
+            yield return new object[] { new int[] { 1, 1, 1, 1, 1 }, 4, new List<IList<int>> {
+            new List<int> { 1, 1, 1, 1 }
+        }};
+            yield return new object[] { new int[] { 1, 0, -1, 0, -2, 2, -1, 1 }, 0, new List<IList<int>> {
+                new List<int> { -2, -1, 1, 2 },
+                new List<int> { -2, 0, 0, 2 },
+                new List<int> { -1, -1, 1, 1 },
+                new List<int> { -1, 0, 0, 1 },
+                new List<int> {-2, 0, 1, 1 },
+                new List<int> {-1, -1, 0, 2 }
+            }};
+        }
+
+        [Theory]
+        [MemberData(nameof(FourSumTestData))]
+        public void FourSum_Test(int[] nums, int target, IList<IList<int>> expected)
+        {
+            var solution = new FourSumSolution();
+            IList<IList<int>> result = solution.FourSum(nums, target);
+            Assert.Equal(expected.Count, result.Count);
+            var sortedExpected = expected.Select(list => list.OrderBy(x => x).ToList()).OrderBy(list => string.Join(",", list)).ToList();
+            var sortedResult = result.Select(list => list.OrderBy(x => x).ToList()).OrderBy(list => string.Join(",", list)).ToList();
+            for (int i = 0; i < sortedExpected.Count; i++)
+            {
+                Assert.Equal(sortedExpected[i], sortedResult[i]);
+            }
+        }
+    }
+}
