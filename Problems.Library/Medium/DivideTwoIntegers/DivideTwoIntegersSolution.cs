7c6078009d6b958055a0b289dc572cab24076091
Parminder Singh <parmindersingh.swe@gmail.com>
2024-05-27 14:36:17 -0300

    Divide Two Integers

---
+++ Problems.Library/Medium/DivideTwoIntegers/DivideTwoIntegersSolution.cs
@@ -0,0 +1,39 @@
+﻿namespace Problems.Library.Medium.DivideTwoIntegers
+{
+    /// <summary>
+    /// Solution to divide two integers without using multiplication, division, and mod operator.
+    /// </summary>
+    public class DivideTwoIntegersSolution
+    {
+        /// <summary>
+        /// Divides two integers and returns the quotient.
+        /// </summary>
+        /// <param name="dividend">The number to be divided.</param>
+        /// <param name="divisor">The number by which to divide.</param>
+        /// <returns>The quotient of the division.</returns>
+        public int Divide(int dividend, int divisor)
+        {
+            if (dividend == int.MinValue && divisor == -1)
+                return int.MaxValue;
+
+            int sign = (dividend < 0) == (divisor < 0) ? 1 : -1;
+            long absDividend = Math.Abs((long)dividend);
+            long absDivisor = Math.Abs((long)divisor);
+            long result = 0;
+
+            while (absDividend >= absDivisor)
+            {
+                long temp = absDivisor, increment = 1;
+                while ((temp << 1) <= absDividend)
+                {
+                    temp <<= 1;
+                    increment <<= 1;
+                }
+                absDividend -= temp;
+                result += increment;
+            }
+            result *= sign;
+            return (int)result;
+        }
+    }
+}
---
+++ Problems.Test/Medium/DivideTwoIntegersTests.cs
@@ -0,0 +1,40 @@
+﻿using Problems.Library.Medium.DivideTwoIntegers;
+
+namespace Problems.Test.Medium
+{
+    public class DivideTwoIntegersTests
+    {
+        public static IEnumerable<object[]> DivideTestData()
+        {
+            yield return new object[] { 10, 3, 3 };
+            yield return new object[] { 7, -3, -2 };
+            yield return new object[] { 0, 1, 0 };
+            yield return new object[] { 1, 1, 1 };
+            yield return new object[] { -1, 1, -1 };
+            yield return new object[] { Int32.MinValue, -1, Int32.MaxValue };
+            yield return new object[] { Int32.MinValue, 1, Int32.MinValue };
+            yield return new object[] { Int32.MaxValue, 1, Int32.MaxValue };
+            yield return new object[] { -10, 2, -5 };
+            yield return new object[] { 100, 10, 10 };
+            yield return new object[] { -100, -10, 10 };
+            yield return new object[] { 15, 2, 7 };
+            yield return new object[] { 1, -1, -1 };
+            yield return new object[] { -1, -1, 1 };
+            yield return new object[] { 100, -3, -33 };
+            yield return new object[] { Int32.MinValue, Int32.MaxValue, -1 }; 
+            yield return new object[] { 5, Int32.MaxValue, 0 };
+            yield return new object[] { Int32.MaxValue, Int32.MinValue, 0 };
+            yield return new object[] { 0, -1, 0 };
+            yield return new object[] { -1, 2, 0 };
+        }
+
+        [Theory]
+        [MemberData(nameof(DivideTestData))]
+        public void DivideTwoIntegers_Test(int dividend, int divisor, int expected)
+        {
+            var solution = new DivideTwoIntegersSolution();
+            int result = solution.Divide(dividend, divisor);
+            Assert.Equal(expected, result);
+        }
+    }
+}
