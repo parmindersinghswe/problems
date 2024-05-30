7911ce32de80391eae9abd2343ba4492f49bbd21
Parminder Singh <parmindersingh.swe@gmail.com>
2024-05-29 22:35:41 -0300

    Three Sum

---
+++ Problems.Library/Medium/ThreeSum/ThreeSumSolution.cs
@@ -0,0 +1,50 @@
+﻿namespace Problems.Library.Medium.ThreeSum
+{
+    /// <summary>
+    /// Solution to find all unique triplets in the array which give the sum of zero.
+    /// </summary>
+    public class ThreeSumSolution
+    {
+        /// <summary>
+        /// Finds all unique triplets in the array which give the sum of zero.
+        /// </summary>
+        /// <param name="nums">The input array of integers.</param>
+        /// <returns>A list of lists, where each list contains three integers that sum up to zero.</returns>
+        public IList<IList<int>> ThreeSum(int[] nums, int target)
+        {
+            Array.Sort(nums);
+            IList<IList<int>> result = new List<IList<int>>();
+
+            for (int i = 0; i < nums.Length - 2; i++)
+            {
+                if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+                int left = i + 1;
+                int right = nums.Length - 1;
+
+                while (left < right)
+                {
+                    int sum = nums[i] + nums[left] + nums[right];
+                    if (sum == target)
+                    {
+                        result.Add(new List<int> { nums[i], nums[left], nums[right] });
+                        while (left < right && nums[left] == nums[left + 1]) left++;
+                        while (left < right && nums[right] == nums[right - 1]) right--;
+                        left++;
+                        right--;
+                    }
+                    else if (sum < 0)
+                    {
+                        left++;
+                    }
+                    else
+                    {
+                        right--;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Problems.Test/Medium/ThreeSumTests.cs
@@ -0,0 +1,43 @@
+﻿using Problems.Library.Medium.ThreeSum;
+
+namespace Problems.Test.Medium
+{
+    public class ThreeSumTests
+    {
+        public static IEnumerable<object[]> ThreeSumTestData()
+        {
+            yield return new object[] { new int[] { -1, 0, 1, 2, -1, -4 }, new List<IList<int>> {
+            new List<int> { -1, -1, 2 },
+            new List<int> { -1, 0, 1 }
+        }};
+            yield return new object[] { new int[] { 0, 0, 0 }, new List<IList<int>> {
+            new List<int> { 0, 0, 0 }
+        }};
+            yield return new object[] { new int[] { 1, -1, -1, 0 }, new List<IList<int>> {
+            new List<int> { -1, 0, 1 }
+        }};
+            yield return new object[] { new int[] { -2, 0, 1, 1, 2 }, new List<IList<int>> {
+            new List<int> { -2, 0, 2 },
+            new List<int> { -2, 1, 1 }
+        }};
+            yield return new object[] { new int[] { -1, 0, 1, 0 }, new List<IList<int>> {
+            new List<int> { -1, 0, 1 }
+        }};
+            yield return new object[] { new int[] { }, new List<IList<int>>() };
+            yield return new object[] { new int[] { 0 }, new List<IList<int>>() };
+        }
+
+        [Theory]
+        [MemberData(nameof(ThreeSumTestData))]
+        public void ThreeSum_Test(int[] nums, IList<IList<int>> expected)
+        {
+            var solution = new ThreeSumSolution();
+            IList<IList<int>> result = solution.ThreeSum(nums, 0);
+            Assert.Equal(expected.Count, result.Count);
+            for (int i = 0; i < expected.Count; i++)
+            {
+                Assert.Equal(expected[i], result[i]);
+            }
+        }
+    }
+}
