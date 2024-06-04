928250f46e4010cd5386c389f390597e699011c1
Parminder Singh <parmindersingh.swe@gmail.com>
2024-06-03 23:44:40 -0300

    Symmetric Tree

---
+++ Problems.Library/Easy/SymmetricTree/SymmetricTreeSolution.cs
@@ -0,0 +1,27 @@
+﻿using Problems.Library.Models;
+
+namespace Problems.Library.Easy.SymmetricTree
+{
+    public class SymmetricTreeSolution
+    {
+        // Method to check if a tree is symmetric
+        public bool IsSymmetric(TreeNode root)
+        {
+            // If the tree is empty, it's symmetric by definition
+            return root == null || IsMirror(root.left, root.right);
+        }
+
+        // Helper method to check if two subtrees are mirror images of each other
+        public bool IsMirror(TreeNode? left, TreeNode? right)
+        {
+            // If either subtree is null, both must be null to be mirrors
+            if (left == null || right == null)
+            {
+                return left == right;
+            }
+            // Check if the current nodes have the same value and their children are mirrors
+            return left.val == right.val && IsMirror(left.left, right.right) && IsMirror(left.right, right.left);
+        }
+    }
+
+}
---
+++ Problems.Test/Easy/SymmetricTreeTests.cs
@@ -0,0 +1,50 @@
+﻿using Problems.Library.Models;
+using Problems.Library.Easy.SymmetricTree;
+
+namespace Problems.Test.Easy
+{
+    public class SymmetricTreeTests
+    {
+        public static IEnumerable<object[]> GetTestData()
+        {            var symmetricTree = new TreeNode(1,
+                new TreeNode(2,
+                    new TreeNode(3),
+                    new TreeNode(4)
+                ),
+                new TreeNode(2,
+                    new TreeNode(4),
+                    new TreeNode(3)
+                )
+            );
+            var asymmetricTree = new TreeNode(1,
+                new TreeNode(2,
+                    null,
+                    new TreeNode(3)
+                ),
+                new TreeNode(2,
+                    null,
+                    new TreeNode(3)
+                )
+            );
+
+            var singleNodeTree = new TreeNode(1);
+
+            TreeNode nullTree = null;
+
+            return new List<object[]> {
+            new object[] { symmetricTree, true },
+            new object[] { asymmetricTree, false },
+            new object[] { singleNodeTree, true },
+            new object[] { nullTree, true }
+        };
+        }
+
+        [Theory]
+        [MemberData(nameof(GetTestData))]
+        public void IsSymmetric_TestCases(TreeNode root, bool expected)
+        {
+            var solution = new SymmetricTreeSolution();
+            Assert.Equal(expected, solution.IsSymmetric(root));
+        }
+    }
+}
