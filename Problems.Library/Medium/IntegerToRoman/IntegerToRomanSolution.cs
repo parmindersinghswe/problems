9b4158c220a73d4fac8e457116546e0c60fb6882
Parminder Singh <parmindersingh.swe@gmail.com>
2024-06-04 21:47:30 -0300

    Integer To Roman

---
+++ Problems.Library/Medium/IntegerToRoman/IntegerToRomanSolution.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+
+namespace Problems.Library.Medium.IntegerToRoman
+{
+    /// <summary>
+    /// Class to convert an integer to its Roman numeral representation.
+    /// </summary>
+    public class IntegerToRomanSolution
+    {
+        /// <summary>
+        /// Array of tuples that map integer values to their corresponding Roman numeral representations.
+        /// </summary>
+        private static readonly (int value, string numeral)[] RomanNumerals = new (int, string)[]
+        {
+            (1000, "M"),
+            (900, "CM"),
+            (500, "D"),
+            (400, "CD"),
+            (100, "C"),
+            (90, "XC"),
+            (50, "L"),
+            (40, "XL"),
+            (10, "X"),
+            (9, "IX"),
+            (5, "V"),
+            (4, "IV"),
+            (1, "I")
+        };
+
+        /// <summary>
+        /// Converts an integer to its Roman numeral representation.
+        /// </summary>
+        /// <param name="num">The integer to convert.</param>
+        /// <returns>A string representing the Roman numeral of the given integer.</returns>
+        public string IntToRoman(int num)
+        {
+            StringBuilder roman = new StringBuilder();
+            foreach (var (value, numeral) in RomanNumerals)
+            {
+                while (num >= value)
+                {
+                    roman.Append(numeral);
+                    num -= value;
+                }
+            }
+            return roman.ToString();
+        }
+    }
+}
---
+++ Problems.Test/Medium/IntegerToRomanTests.cs
@@ -0,0 +1,28 @@
+﻿using Problems.Library.Medium.IntegerToRoman;
+namespace Problems.Test.Medium
+{
+    public class IntegerToRomanTests
+    {
+        public static IEnumerable<object[]> GetTestData()
+        {
+            return new List<object[]>
+            {
+                new object[] { 1, "I" },
+                new object[] { 4, "IV" },
+                new object[] { 9, "IX" },
+                new object[] { 58, "LVIII" },
+                new object[] { 1994, "MCMXCIV" },
+                new object[] { 3999, "MMMCMXCIX" }
+            };
+        }
+
+        [Theory]
+        [MemberData(nameof(GetTestData))]
+        public void IntegerToRoman_Tests(int num, string expected)
+        {
+            var solution = new IntegerToRomanSolution();
+            Assert.Equal(expected, solution.IntToRoman(num));
+        }
+
+    }
+}
