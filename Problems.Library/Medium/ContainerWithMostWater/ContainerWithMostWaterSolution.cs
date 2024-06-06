be05be062dfeddcce23911e12ca8b916e706bf7c
Parminder Singh <parmindersingh.swe@gmail.com>
2024-06-06 20:29:03 -0300

    Container With Most Water

---
+++ Problems.Library/Medium/ContainerWithMostWater/ContainerWithMostWaterSolution.cs
@@ -0,0 +1,38 @@
+﻿namespace Problems.Library.Medium.MaxArea
+{
+    /// <summary>
+    /// Class to solve the problem of finding the maximum area of water that can be contained.
+    /// </summary>
+    public class ContainerWithMostWaterSolution
+    {
+        /// <summary>
+        /// Finds the maximum area of water that can be contained by the given heights.
+        /// </summary>
+        /// <param name="height">An array of integers representing the heights.</param>
+        /// <returns>The maximum area of water that can be contained.</returns>
+        public int MaxArea(int[] height)
+        {
+            int left = 0, right = height.Length - 1;
+            int max = 0;
+
+            while (left < right)
+            {
+                int currentHeight = Math.Min(height[left], height[right]);
+                int currentWidth = right - left;
+                int currentArea = currentHeight * currentWidth;
+                max = Math.Max(max, currentArea);
+
+                if (height[left] < height[right])
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+            }
+
+            return max;
+        }
+    }
+}
---
+++ Problems.Test/Medium/ContainerWithMostWaterTests.cs
@@ -0,0 +1,29 @@
+﻿using Problems.Library.Medium.MaxArea;
+
+namespace Problems.Test.Medium
+{
+    public class ContainerWithMostWaterTests
+    {
+        public static IEnumerable<object[]> GetTestData()
+        {
+            return new List<object[]> {
+            new object[] { new int[] {1, 8, 6, 2, 5, 4, 8, 3, 7}, 49 },
+            new object[] { new int[] {1, 1}, 1 },
+            new object[] { new int[] {4, 3, 2, 1, 4}, 16 },
+            new object[] { new int[] {1, 2, 1}, 2 },
+            new object[] { new int[] {1, 3, 2, 5, 25, 24, 5}, 24 },
+            new object[] { new int[] {10, 1, 1, 1, 10}, 40 },
+            new object[] { new int[] {1, 1, 1, 1, 1, 1, 1, 1}, 7 },
+            new object[] { new int[] {3, 9, 3, 4, 7, 2, 12, 6}, 45 }
+        };
+        }
+
+        [Theory]
+        [MemberData(nameof(GetTestData))]
+        public void MaxArea_Tests(int[] height, int expected)
+        {
+            var solution = new ContainerWithMostWaterSolution();
+            Assert.Equal(expected, solution.MaxArea(height));
+        }
+    }
+}
