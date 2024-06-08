74b12996004ea99464d6e9aa7812e3ec20c0cae6
Parminder Singh <parmindersingh.swe@gmail.com>
2024-06-08 17:37:15 -0300

    Next Permutation

---
+++ Problems.Library/Medium/NextPermutation/NextPermutationSolution.cs
@@ -0,0 +1,62 @@
+﻿namespace Problems.Library.Medium.NextPermutation
+{
+    /// <summary>
+    /// Class to solve the problem of finding the next lexicographical permutation of a sequence.
+    /// </summary>
+    public class NextPermutationSolution
+    {
+        /// <summary>
+        /// Rearranges numbers into the lexicographically next greater permutation of numbers.
+        /// </summary>
+        /// <param name="nums">An array of integers to find the next permutation for.</param>
+        public void NextPermutation(int[] nums)
+        {
+            int length = nums.Length;
+            int i = length - 2;
+            while (i >= 0 && nums[i] >= nums[i + 1])
+            {
+                i--;
+            }
+
+            if (i >= 0)
+            {
+                int j = length - 1;
+                while (nums[j] <= nums[i])
+                {
+                    j--;
+                }
+                Swap(nums, i, j);
+            }
+            Reverse(nums, i + 1, length - 1);
+        }
+
+        /// <summary>
+        /// Swaps the values at two specified indices in an array.
+        /// </summary>
+        /// <param name="nums">The array of integers.</param>
+        /// <param name="i">The first index.</param>
+        /// <param name="j">The second index.</param>
+        private void Swap(int[] nums, int i, int j)
+        {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+
+        /// <summary>
+        /// Reverses the order of elements in a portion of an array.
+        /// </summary>
+        /// <param name="nums">The array of integers.</param>
+        /// <param name="start">The starting index of the portion to reverse.</param>
+        /// <param name="end">The ending index of the portion to reverse.</param>
+        private void Reverse(int[] nums, int start, int end)
+        {
+            while (start < end)
+            {
+                Swap(nums, start, end);
+                start++;
+                end--;
+            }
+        }
+    }
+}
---
+++ Problems.Test/Medium/NextPermutationTests.cs
@@ -0,0 +1,38 @@
+﻿using Problems.Library.Medium.NextPermutation;
+
+namespace Problems.Test.Medium
+{
+    public class NextPermutationTests
+    {
+        public static IEnumerable<object[]> GetTestData()
+        {
+            return new List<object[]> {
+            new object[] { new int[] {1, 2, 3}, new int[] {1, 3, 2} },
+            new object[] { new int[] {3, 2, 1}, new int[] {1, 2, 3} },
+            new object[] { new int[] {1, 1, 5}, new int[] {1, 5, 1} },
+            new object[] { new int[] {1, 3, 2}, new int[] {2, 1, 3} },
+            new object[] { new int[] {2, 3, 1}, new int[] {3, 1, 2} },
+            new object[] { new int[] {1, 5, 1}, new int[] {5, 1, 1} },
+            new object[] { new int[] {2, 4, 3, 2, 1}, new int[] {3, 1, 2, 2, 4} },
+            new object[] { new int[] {1, 4, 3, 2}, new int[] {2, 1, 3, 4} },
+            new object[] { new int[] {1, 3, 5, 4, 2}, new int[] {1, 4, 2, 3, 5} },
+            new object[] { new int[] {1, 1, 1, 2}, new int[] {1, 1, 2, 1} },
+            new object[] { new int[] {1}, new int[] {1} },
+            new object[] { new int[] {1, 2, 4, 3}, new int[] {1, 3, 2, 4} },
+            new object[] { new int[] {4, 2, 0, 2, 3, 2, 0}, new int[] {4, 2, 0, 3, 0, 2, 2} },
+            new object[] { new int[] {2, 3, 1, 3, 3}, new int[] {2, 3, 3, 1, 3} },
+            new object[] { new int[] {3, 2, 1, 4, 3}, new int[] {3, 2, 3, 1, 4} },
+            new object[] { new int[] {1, 5, 8, 4, 7, 6, 5, 3, 1}, new int[] {1, 5, 8, 5, 1, 3, 4, 6, 7} }
+        };
+        }
+
+        [Theory]
+        [MemberData(nameof(GetTestData))]
+        public void NextPermutation_TestCases(int[] nums, int[] expected)
+        {
+            var solution = new NextPermutationSolution();
+            solution.NextPermutation(nums);
+            Assert.Equal(expected, nums);
+        }
+    }
+}
