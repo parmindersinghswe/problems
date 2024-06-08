d88686115ab327f67619b57952d9743e49a6cbc8
Parminder Singh <parmindersingh.swe@gmail.com>
2024-06-08 20:59:45 -0300

    Subsets

---
+++ Problems.Library/Medium/Subsets/SubsetsSolution.cs
@@ -0,0 +1,39 @@
+﻿namespace Problems.Library.Medium.Subsets
+{
+    /// <summary>
+    /// Solution to generate all possible subsets of a given array of integers.
+    /// </summary>
+    public class SubsetsSolution
+    {
+        /// <summary>
+        /// Generates all possible subsets of the given array of integers.
+        /// </summary>
+        /// <param name="nums">The array of integers.</param>
+        /// <returns>A list of lists, where each list is a subset of the input array.</returns>
+        public IList<IList<int>> Subsets(int[] nums)
+        {
+            IList<IList<int>> list = new List<IList<int>>();
+            GetUniqueSubsets(nums, list, new List<int>(), 0, nums.Length);
+            return list;
+        }
+
+        /// <summary>
+        /// Recursively generates all unique subsets of the given array.
+        /// </summary>
+        /// <param name="nums">The array of integers.</param>
+        /// <param name="list">The list to store all subsets.</param>
+        /// <param name="currentSet">The current subset being constructed.</param>
+        /// <param name="currentIndex">The current index in the array.</param>
+        /// <param name="length">The length of the array.</param>
+        public void GetUniqueSubsets(int[] nums, IList<IList<int>> list, IList<int> currentSet, int currentIndex, int length)
+        {
+            list.Add(new List<int>(currentSet));
+            for (int i = currentIndex; i < length; i++)
+            {
+                currentSet.Add(nums[i]);
+                GetUniqueSubsets(nums, list, currentSet, i + 1, length);
+                currentSet.RemoveAt(currentSet.Count - 1);
+            }
+        }
+    }
+}
---
+++ Problems.Test/Medium/SubsetsTests.cs
@@ -0,0 +1,134 @@
+﻿using Problems.Library.Medium.Subsets;
+namespace Problems.Test.Medium
+{
+    public class SubsetsTests
+    {
+        [Theory]
+        [MemberData(nameof(GetTestData))]
+        public void Subsets_TestCases(int[] nums, IList<IList<int>> expected)
+        {
+            var solution = new SubsetsSolution();
+            var result = solution.Subsets(nums);
+            Assert.Equal(expected.Count, result.Count);
+            foreach (var subset in expected)
+            {
+                Assert.Contains(subset, result);
+            }
+        }
+
+        public static IEnumerable<object[]> GetTestData()
+        {
+            return new List<object[]>{
+          new object[]{
+              new int[]{1, 2, 3},
+              new List<IList<int>>{
+                  new List<int>(), new List<int>{1}, new List<int>{2},
+                  new List<int>{3}, new List<int>{1, 2}, new List<int>{1, 3},
+                  new List<int>{2, 3}, new List<int>{1, 2, 3}}},
+          new object[]{new int[]{0},
+                       new List<IList<int>>{new List<int>(), new List<int>{0}}},
+          new object[]{new int[]{}, new List<IList<int>>{new List<int>()}},
+          new object[]{
+              new int[]{1, 2},
+              new List<IList<int>>{new List<int>(), new List<int>{1},
+                                   new List<int>{2}, new List<int>{1, 2}}},
+          new object[]{new int[]{4, 5, 6},
+                       new List<IList<int>>{
+                           new List<int>(),
+                           new List<int>{4}, new List<int>{5}, new List<int>{6},
+                           new List<int>{4, 5}, new List<int>{4, 6},
+                           new List<int>{5, 6}, new List<int>{4, 5, 6}}},
+          new object[]{
+              new int[]{7, 8, 9, 10},
+              new List<IList<int>>{
+                  new List<int>(),
+                  new List<int>{7}, new List<int>{8}, new List<int>{9},
+                  new List<int>{10}, new List<int>{7, 8}, new List<int>{7, 9},
+                  new List<int>{7, 10}, new List<int>{8, 9},
+                  new List<int>{8, 10}, new List<int>{9, 10},
+                  new List<int>{7, 8, 9}, new List<int>{7, 8, 10},
+                  new List<int>{7, 9, 10}, new List<int>{8, 9, 10},
+                  new List<int>{7, 8, 9, 10}}},
+          new object[]{
+              new int[]{-1, -2, -3},
+              new List<IList<int>>{new List<int>(), new List<int>{-1},
+                                   new List<int>{-2}, new List<int>{-3},
+                                   new List<int>{-1, -2}, new List<int>{-1, -3},
+                                   new List<int>{-2, -3},
+                                   new List<int>{-1, -2, -3}}},
+          new object[]{
+              new int[]{0, 1, 2, 3},
+              new List<IList<int>>{
+                  new List<int>(),
+                  new List<int>{0}, new List<int>{1}, new List<int>{2},
+                  new List<int>{3}, new List<int>{0, 1}, new List<int>{0, 2},
+                  new List<int>{0, 3}, new List<int>{1, 2}, new List<int>{1, 3},
+                  new List<int>{2, 3}, new List<int>{0, 1, 2},
+                  new List<int>{0, 1, 3}, new List<int>{0, 2, 3},
+                  new List<int>{1, 2, 3}, new List<int>{0, 1, 2, 3}}},
+          new object[]{
+              new int[]{100},
+              new List<IList<int>>{new List<int>(), new List<int>{100}}},
+          new object[]{
+              new int[]{1, 1},
+              new List<IList<int>>{new List<int>(), new List<int>{1},
+                                   new List<int>{1}, new List<int>{1, 1}}},
+          new object[]{new int[]{2, 3, 4},
+                       new List<IList<int>>{
+                           new List<int>{},
+                           new List<int>{2}, new List<int>{3}, new List<int>{4},
+                           new List<int>{2, 3}, new List<int>{2, 4},
+                           new List<int>{3, 4}, new List<int>{2, 3, 4}}},
+          new object[]{new int[]{1, 2, 2},
+                       new List<IList<int>>{
+                           new List<int>{},
+                           new List<int>{1}, new List<int>{2}, new List<int>{2},
+                           new List<int>{1, 2}, new List<int>{1, 2},
+                           new List<int>{2, 2}, new List<int>{1, 2, 2}}},
+          new object[]{new int[]{1, 1, 2},
+                       new List<IList<int>>{
+                           new List<int>{},
+                           new List<int>{1}, new List<int>{1}, new List<int>{2},
+                           new List<int>{1, 1}, new List<int>{1, 2},
+                           new List<int>{1, 2}, new List<int>{1, 1, 2}}},
+          new object[]{new int[]{2, 2, 2},
+                       new List<IList<int>>{
+                           new List<int>{},
+                           new List<int>{2}, new List<int>{2}, new List<int>{2},
+                           new List<int>{2, 2}, new List<int>{2, 2},
+                           new List<int>{2, 2}, new List<int>{2, 2, 2}}},
+          new object[]{new int[]{3, 2, 1},
+                       new List<IList<int>>{
+                           new List<int>{},
+                           new List<int>{3}, new List<int>{2}, new List<int>{1},
+                           new List<int>{3, 2}, new List<int>{3, 1},
+                           new List<int>{2, 1}, new List<int>{3, 2, 1}}},
+          new object[]{new int[]{5, 5, 5},
+                       new List<IList<int>>{
+                           new List<int>{},
+                           new List<int>{5}, new List<int>{5}, new List<int>{5},
+                           new List<int>{5, 5}, new List<int>{5, 5},
+                           new List<int>{5, 5}, new List<int>{5, 5, 5}}},
+          new object[]{
+              new int[]{1, 2, 3, 4, 5},
+              new List<IList<int>>{
+                  new List<int>{},           new List<int>{1},
+                  new List<int>{2},          new List<int>{3},
+                  new List<int>{4},          new List<int>{5},
+                  new List<int>{1, 2},       new List<int>{1, 3},
+                  new List<int>{1, 4},       new List<int>{1, 5},
+                  new List<int>{2, 3},       new List<int>{2, 4},
+                  new List<int>{2, 5},       new List<int>{3, 4},
+                  new List<int>{3, 5},       new List<int>{4, 5},
+                  new List<int>{1, 2, 3},    new List<int>{1, 2, 4},
+                  new List<int>{1, 2, 5},    new List<int>{1, 3, 4},
+                  new List<int>{1, 3, 5},    new List<int>{1, 4, 5},
+                  new List<int>{2, 3, 4},    new List<int>{2, 3, 5},
+                  new List<int>{2, 4, 5},    new List<int>{3, 4, 5},
+                  new List<int>{1, 2, 3, 4}, new List<int>{1, 2, 3, 5},
+                  new List<int>{1, 2, 4, 5}, new List<int>{1, 3, 4, 5},
+                  new List<int>{2, 3, 4, 5}, new List<int>{1, 2, 3, 4, 5}}}
+            };
+        }
+    }
+}
