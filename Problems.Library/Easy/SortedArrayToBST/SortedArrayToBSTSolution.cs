1bef8c740200676cf411ec337cf42a4b6b3ff25f
Parminder Singh <parmindersingh.swe@gmail.com>
2024-06-08 18:03:58 -0300

    Sorted Array To BST

---
+++ Problems.Library/Easy/SortedArrayToBST/SortedArrayToBSTSolution.cs
@@ -0,0 +1,37 @@
+﻿using Problems.Library.Models;
+
+namespace Problems.Library.Easy.SortedArrayToBST
+{
+    /// <summary>
+    /// Class to convert a sorted array to a height-balanced binary search tree (BST).
+    /// </summary>
+    public class SortedArrayToBSTSolution
+    {
+        /// <summary>
+        /// Converts a sorted array to a height-balanced BST.
+        /// </summary>
+        /// <param name="nums">The sorted array of integers.</param>
+        /// <returns>The root node of the height-balanced BST.</returns>
+        public TreeNode? SortedArrayToBST(int[] nums)
+        {
+            return GetNode(nums, 0, nums.Length - 1);
+        }
+
+        /// <summary>
+        /// Recursively constructs the BST from the sorted array.
+        /// </summary>
+        /// <param name="nums">The sorted array of integers.</param>
+        /// <param name="left">The left boundary index.</param>
+        /// <param name="right">The right boundary index.</param>
+        /// <returns>The root node of the subtree.</returns>
+        private TreeNode? GetNode(int[] nums, int left, int right)
+        {
+            if (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+                return new TreeNode(nums[mid], GetNode(nums, left, mid - 1), GetNode(nums, mid + 1, right));
+            }
+            return null;
+        }
+    }
+}
---
+++ Problems.Test/Easy/SortedArrayToBSTTests.cs
@@ -0,0 +1,36 @@
+﻿using Problems.Library.Models;
+using Problems.Library.Easy.SortedArrayToBST;
+using Problems.Library.Easy.BinaryTreeTraversal;
+
+namespace Problems.Test.Easy
+{
+    public class SortedArrayToBSTTests
+    {
+        public static IEnumerable<object[]> GetTestData()
+        {
+            return new List<object[]> {
+            new object[] { new int[] {-10, -3, 0, 5, 9}, new int[] {-10, -3, 0, 5, 9} },
+            new object[] { new int[] {1, 3}, new int[] {1, 3} },
+            new object[] { new int[] {}, new int[] {} },
+            new object[] { new int[] {1}, new int[] {1} },
+            new object[] { new int[] {1, 2, 3, 4, 5, 6, 7}, new int[] {1, 2, 3, 4, 5, 6, 7} },
+            new object[] { new int[] {1, 2, 3, 4, 5, 6}, new int[] {1, 2, 3, 4, 5, 6} },
+            new object[] { new int[] {-5, -4, -3, -2, -1}, new int[] {-5, -4, -3, -2, -1} },
+            new object[] { new int[] {100, 200, 300, 400, 500}, new int[] {100, 200, 300, 400, 500} },
+            new object[] { new int[] {1, 2, 2, 3, 3, 4, 5, 5, 6, 7, 8}, new int[] {1, 2, 2, 3, 3, 4, 5, 5, 6, 7, 8} },
+            new object[] { new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10} },
+        };
+        }
+
+        [Theory]
+        [MemberData(nameof(GetTestData))]
+        public void SortedArrayToBST_TestCases(int[] nums, int[] expectedInOrder)
+        {
+            var solution = new SortedArrayToBSTSolution();
+            TreeNode? root = solution.SortedArrayToBST(nums);
+            var inorderSolution = new BinaryTreeTraversalSolution();
+            var resultInOrder = inorderSolution.InorderTraversal(root);
+            Assert.Equal(expectedInOrder, resultInOrder);
+        }
+    }
+}
