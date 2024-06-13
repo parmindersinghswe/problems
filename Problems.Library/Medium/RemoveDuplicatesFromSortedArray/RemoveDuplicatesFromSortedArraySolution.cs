1e290e6819e34a57ab16d3f05089ed2f3629dce1
Parminder Singh <parmindersingh.swe@gmail.com>
2024-06-13 20:26:30 -0300

    Remove Duplicates from Sorted Array II

---
+++ Problems.Library/Medium/RemoveDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArraySolution.cs
@@ -0,0 +1,40 @@
+﻿namespace Problems.Library.Medium.RemoveDuplicatesFromSortedArray
+{
+    /// <summary>
+    /// Solution to remove duplicates from a sorted array allowing at most two occurrences of each element.
+    /// </summary>
+    public class RemoveDuplicatesFromSortedArraySolution
+    {
+        /// <summary>
+        /// Removes duplicates from a sorted array, allowing at most two occurrences of each element.
+        /// </summary>
+        /// <param name="nums">The sorted array of integers.</param>
+        /// <returns>The length of the array after removing the duplicates.</returns>
+        public int RemoveDuplicates(int[] nums)
+        {
+            int length = nums.Length;
+            if (length <= 2) return length;
+
+            int duplicate = 1;
+            int currentIndex = 1;
+
+            for (int i = 1; i < length; i++)
+            {
+                if (nums[i] == nums[i - 1])
+                {
+                    duplicate++;
+                }
+                else
+                {
+                    duplicate = 1;
+                }
+
+                if (duplicate <= 2)
+                {
+                    nums[currentIndex++] = nums[i];
+                }
+            }
+            return currentIndex;
+        }
+    }
+}
---
+++ Problems.Test/Medium/RemoveDuplicatesFromSortedArrayTests.cs
@@ -0,0 +1,32 @@
+﻿using Problems.Library.Medium.RemoveDuplicatesFromSortedArray;
+
+namespace Problems.Test.Medium
+{
+    public class RemoveDuplicatesFromSortedArrayTests
+    {
+        [Theory]
+        [MemberData(nameof(GetTestData))]
+        public void RemoveDuplicates_Tests(int[] nums, int expectedLength, int[] expectedNums)
+        {
+            var solution = new RemoveDuplicatesFromSortedArraySolution();
+            int resultLength = solution.RemoveDuplicates(nums);
+            Assert.Equal(expectedLength, resultLength);
+            for (int i = 0; i < expectedLength; i++)
+            {
+                Assert.Equal(expectedNums[i], nums[i]);
+            }
+        }
+
+        public static IEnumerable<object[]> GetTestData()
+        {
+            return new List<object[]> {
+            new object[] { new int[] {1, 1, 1, 2, 2, 3}, 5, new int[] {1, 1, 2, 2, 3} },
+            new object[] { new int[] {0, 0, 1, 1, 1, 1, 2, 3, 3}, 7, new int[] {0, 0, 1, 1, 2, 3, 3} },
+            new object[] { new int[] {}, 0, new int[] {} },
+            new object[] { new int[] {1}, 1, new int[] {1} },
+            new object[] { new int[] {1, 1, 2, 2, 2, 3}, 5, new int[] {1, 1, 2, 2, 3} },
+            new object[] { new int[] {1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 4}, 7, new int[] {1, 2, 2, 3, 3, 4, 4} }
+        };
+        }
+    }
+}
