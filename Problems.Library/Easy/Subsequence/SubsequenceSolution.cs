bb272a91442b81f78749124c7ba903651c94d832
Parminder Singh <parmindersingh.swe@gmail.com>
2024-06-14 23:53:25 -0300

    Subsequence

---
+++ Problems.Library/Easy/Subsequence/SubsequenceSolution.cs
@@ -0,0 +1,27 @@
+﻿namespace Problems.Library.Easy.Subsequence
+{
+    /// <summary>
+    /// Solution to check if one string is a subsequence of another string.
+    /// </summary>
+    public class SubsequenceSolution
+    {
+        /// <summary>
+        /// Determines whether the string s is a subsequence of string t.
+        /// </summary>
+        /// <param name="s">The subsequence string to check.</param>
+        /// <param name="t">The main string to check against.</param>
+        /// <returns><c>true</c> if s is a subsequence of t; otherwise, <c>false</c>.</returns>
+        public bool IsSubsequence(string s, string t)
+        {
+            int i = 0;
+            for (int j = 0; j < t.Length && i < s.Length; j++)
+            {
+                if (s[i] == t[j])
+                {
+                    i++;
+                }
+            }
+            return (i >= s.Length);
+        }
+    }
+}
---
+++ Problems.Test/Easy/SubsequenceTests.cs
@@ -0,0 +1,36 @@
+﻿using Problems.Library.Easy.Subsequence;
+namespace Problems.Test.Easy
+{
+    public class SubsequenceTests
+    {
+        [Theory]
+        [MemberData(nameof(GetTestData))]
+        public void IsSubsequence_Tests(string s, string t, bool expected)
+        {
+            var solution = new SubsequenceSolution();
+            bool result = solution.IsSubsequence(s, t);
+            Assert.Equal(expected, result);
+        }
+
+        public static IEnumerable<object[]> GetTestData()
+        {
+            return new List<object[]> {
+            new object[] { "abc", "ahbgdc", true },
+            new object[] { "axc", "ahbgdc", false },
+            new object[] { "", "ahbgdc", true },
+            new object[] { "abc", "", false },
+            new object[] { "", "", true },
+            new object[] { "ace", "abcde", true },
+            new object[] { "aec", "abcde", false },
+            new object[] { "aaaa", "bbaaaa", true },
+            new object[] { "acb", "ahbgdc", false },
+            new object[] { "a", "b", false },
+            new object[] { "a", "a", true },
+            new object[] { "a", "ba", true },
+            new object[] { "abc", "abcdabc", true },
+            new object[] { "abc", "abdabac", true },
+            new object[] { "abc", "bacab", false }
+        };
+        }
+    }
+}
