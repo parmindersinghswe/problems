ee971c8a5b7e466311c2341b1dd18c542cf5211c
Parminder Singh <parmindersingh.swe@gmail.com>
2024-06-16 23:05:50 -0300

    Rotate Array

---
+++ Problems.Library/Medium/RotateArray/RotateArraySolution.cs
@@ -0,0 +1,41 @@
+﻿namespace Problems.Library.Medium.RotateArray
+{
+    /// <summary>
+    /// Solution to rotate an array to the right by a given number of steps.
+    /// </summary>
+    public class RotateArraySolution
+    {
+        /// <summary>
+        /// Rotates the array to the right by the specified number of steps.
+        /// </summary>
+        /// <param name="nums">The array of integers to rotate.</param>
+        /// <param name="k">The number of steps to rotate the array.</param>
+        public void Rotate(int[] nums, int k)
+        {
+            k = k % nums.Length;
+            if (k == 0 || nums.Length <= 1) return;
+
+            Reverse(nums, 0, nums.Length - 1);
+            Reverse(nums, 0, k - 1);
+            Reverse(nums, k, nums.Length - 1);
+        }
+
+        /// <summary>
+        /// Reverses the elements in the specified portion of the array.
+        /// </summary>
+        /// <param name="nums">The array of integers.</param>
+        /// <param name="start">The starting index of the portion to reverse.</param>
+        /// <param name="end">The ending index of the portion to reverse.</param>
+        private void Reverse(int[] nums, int start, int end)
+        {
+            while (start < end)
+            {
+                int temp = nums[start];
+                nums[start] = nums[end];
+                nums[end] = temp;
+                start++;
+                end--;
+            }
+        }
+    }
+}
---
+++ Problems.Test/Medium/RotateArrayTests.cs
@@ -0,0 +1,27 @@
+﻿using Problems.Library.Medium.RotateArray;
+namespace Problems.Test.Medium
+{
+    public class RotateArrayTests
+    {
+        [Theory]
+        [MemberData(nameof(GetTestData))]
+        public void Rotate_Tests(int[] nums, int k, int[] expected)
+        {
+            var solution = new RotateArraySolution();
+            solution.Rotate(nums, k);
+            Assert.Equal(expected, nums);
+        }
+
+        public static IEnumerable<object[]> GetTestData()
+        {
+            return new List<object[]> {
+            new object[] { new int[] {1, 2, 3, 4, 5, 6, 7}, 3, new int[] {5, 6, 7, 1, 2, 3, 4} },
+            new object[] { new int[] {-1, -100, 3, 99}, 2, new int[] {3, 99, -1, -100} },
+            new object[] { new int[] {1, 2, 3, 4, 5, 6}, 2, new int[] {5, 6, 1, 2, 3, 4} },
+            new object[] { new int[] {1, 2, 3}, 4, new int[] {3, 1, 2} },
+            new object[] { new int[] {1}, 0, new int[] {1} },
+            new object[] { new int[] {1}, 1, new int[] {1} }
+        };
+        }
+    }
+}
