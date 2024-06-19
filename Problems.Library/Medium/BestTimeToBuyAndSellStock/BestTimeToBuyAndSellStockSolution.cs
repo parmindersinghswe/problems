df7864bd6c104b4b8c45ed09f1c0046850f99b06
Parminder Singh <parmindersingh.swe@gmail.com>
2024-06-18 22:43:18 -0300

    Best Time to Buy and Sell Stock II

---
+++ Problems.Library/Medium/BestTimeToBuyAndSellStock/BestTimeToBuyAndSellStockSolution.cs
@@ -0,0 +1,28 @@
+﻿namespace Problems.Library.Medium.BestTimeToBuyAndSellStock
+{
+    /// <summary>
+    /// Solution for the "Best Time to Buy and Sell Stock" problem.
+    /// </summary>
+    public class BestTimeToBuyAndSellStockSolution
+    {
+        /// <summary>
+        /// Calculates the maximum profit that can be achieved by buying and selling stocks.
+        /// </summary>
+        /// <param name="prices">An array of stock prices where prices[i] is the price of a given stock on the ith day.</param>
+        /// <returns>The maximum profit that can be achieved.</returns>
+        public int MaxProfit(int[] prices)
+        {
+            int maxProfit = 0;
+
+            for (int i = 0; i < prices.Length - 1; i++)
+            {
+                if (prices[i] < prices[i + 1])
+                {
+                    maxProfit += prices[i + 1] - prices[i];
+                }
+            }
+
+            return maxProfit;
+        }
+    }
+}
---
+++ Problems.Test/Medium/BestTimeToBuyAndSellStockTests.cs
@@ -0,0 +1,25 @@
+﻿using Problems.Library.Medium.BestTimeToBuyAndSellStock;
+
+namespace Problems.Test.Medium
+{
+    public class BestTimeToBuyAndSellStockTests
+    {
+        [Theory]
+        [MemberData(nameof(GetTestData))]
+        public void MaxProfit_ShouldReturnExpectedResult(int[] prices, int expectedProfit)
+        {
+            var solution = new BestTimeToBuyAndSellStockSolution();
+            var result = solution.MaxProfit(prices);
+            Assert.Equal(expectedProfit, result);
+        }
+
+        public static IEnumerable<object[]> GetTestData()
+        {
+            yield return new object[] { new int[] { 7, 1, 5, 3, 6, 4 }, 7 }; 
+            yield return new object[] { new int[] { 1, 2, 3, 4, 5 }, 4 };   
+            yield return new object[] { new int[] { 7, 6, 4, 3, 1 }, 0 };   
+            yield return new object[] { new int[] { 1, 2 }, 1 };           
+            yield return new object[] { new int[] { 2, 4, 1 }, 2 };        
+        }
+    }
+}
