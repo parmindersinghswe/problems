43c2055f6161eb7ea8bae0617d90098763da6b2a
Parminder Singh <parmindersingh.swe@gmail.com>
2024-06-19 00:21:03 -0300

    Product of Array Except Self

---
+++ Problems.Library/Medium/ProductOfArrayExceptSelf/ProductOfArrayExceptSelfSolution.cs
@@ -0,0 +1,57 @@
+﻿namespace Problems.Library.Medium.ProductOfArrayExceptSelf
+{
+    /// <summary>
+    /// Solution for the "Product of Array Except Self" problem.
+    /// This class provides a method to calculate the product of an array except for the current element without using division.
+    /// </summary>
+    public class ProductOfArrayExceptSelfSolution
+    {
+        /// <summary>
+        /// Calculates the product of the array except for the current element for each element in the input array.
+        /// </summary>
+        /// <param name="nums">The input array of integers.</param>
+        /// <returns>An array where each element is the product of all the elements of the input array except the current element.</returns>
+        public int[] ProductExceptSelf(int[] nums)
+        {
+            int length = nums.Length;
+            int[] result = new int[length];
+            int product = 1;
+            int zeroCount = 0;
+
+            for (int i = 0; i < length; i++)
+            {
+                if (nums[i] == 0)
+                {
+                    zeroCount++;
+                    if (zeroCount > 1)
+                    {
+                        // If there are more than one zero, all elements in the result are zero
+                        for (int j = 0; j < length; j++)
+                        {
+                            result[j] = 0;
+                        }
+                        return result;
+                    }
+                }
+                else
+                {
+                    product *= nums[i];
+                }
+            }
+
+            for (int i = 0; i < length; i++)
+            {
+                if (zeroCount == 1)
+                {
+                    result[i] = nums[i] == 0 ? product : 0;
+                }
+                else
+                {
+                    result[i] = product / nums[i];
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Problems.Test/Medium/ProductOfArrayExceptSelfTests.cs
@@ -0,0 +1,25 @@
+﻿using Problems.Library.Medium.ProductOfArrayExceptSelf;
+
+namespace Problems.Test.Medium
+{
+    public class ProductOfArrayExceptSelfTests
+    {
+        [Theory]
+        [MemberData(nameof(ProductExceptSelfTestData))]
+        public void ProductExceptSelf_ShouldReturnExpectedResult(int[] nums, int[] expected)
+        {
+            var solution = new ProductOfArrayExceptSelfSolution();
+            var result = solution.ProductExceptSelf(nums);
+            Assert.Equal(expected, result);
+        }
+
+        public static IEnumerable<object[]> ProductExceptSelfTestData()
+        {
+            yield return new object[] { new int[] { 1, 2, 3, 4 }, new int[] { 24, 12, 8, 6 } };
+            yield return new object[] { new int[] { 0, 0 }, new int[] { 0, 0 } };
+            yield return new object[] { new int[] { 0, 4, 0 }, new int[] { 0, 0, 0 } };
+            yield return new object[] { new int[] { 1, 0 }, new int[] { 0, 1 } };
+            yield return new object[] { new int[] { -1, 1, 0, -3, 3 }, new int[] { 0, 0, 9, 0, 0 } };
+        }
+    }
+}
