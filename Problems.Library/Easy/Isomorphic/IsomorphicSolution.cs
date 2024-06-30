b25c09b86bc76f7f5efdeed4bde21a66012e7551
Parminder Singh <parmindersingh.swe@gmail.com>
2024-06-29 23:41:49 -0300

    Isomorphic

---
+++ Problems.Library/Easy/Isomorphic/IsomorphicSolution.cs
@@ -0,0 +1,55 @@
+﻿namespace Problems.Library.Easy.Isomorphic
+{
+    /// <summary>
+    /// Solution class for checking if two strings are isomorphic.
+    /// </summary>
+    public class IsomorphicSolution
+    {
+        /// <summary>
+        /// Determines if two strings are isomorphic.
+        /// </summary>
+        /// <param name="s">The first input string.</param>
+        /// <param name="t">The second input string.</param>
+        /// <returns>True if the strings are isomorphic; otherwise, false.</returns>
+        public bool IsIsomorphic(string s, string t)
+        {
+            if (s.Length != t.Length)
+            {
+                return false;
+            }
+
+            Dictionary<char, char> mapS = new Dictionary<char, char>();
+            Dictionary<char, char> mapT = new Dictionary<char, char>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                char sChar = s[i];
+                char tChar = t[i];
+                if (mapS.ContainsKey(sChar))
+                {
+                    if (mapS[sChar] != tChar)
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    mapS[sChar] = tChar;
+                }
+                if (mapT.ContainsKey(tChar))
+                {
+                    if (mapT[tChar] != sChar)
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    mapT[tChar] = sChar;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Problems.Test/Easy/IsomorphicTests.cs
@@ -0,0 +1,23 @@
+﻿using Problems.Library.Easy.Isomorphic;
+
+namespace Problems.Test.Easy
+{
+    public class IsomorphicTests
+    {
+        [Theory]
+        [InlineData("egg", "add", true)]
+        [InlineData("foo", "bar", false)]
+        [InlineData("paper", "title", true)]
+        [InlineData("abc", "abc", true)]
+        [InlineData("a", "b", true)]
+        [InlineData("", "", true)]
+        [InlineData("abab", "caca", true)]
+        [InlineData("ab", "aa", false)]
+        public void IsIsomorphic_Tests(string s, string t, bool expected)
+        {
+            var solution = new IsomorphicSolution();
+            var result = solution.IsIsomorphic(s, t);
+            Assert.Equal(expected, result);
+        }
+    }
+}
