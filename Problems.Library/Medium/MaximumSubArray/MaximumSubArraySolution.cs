4b3cba7535e98b199c048018de191ef0b3402ec0
Parminder Singh <parmindersingh.swe@gmail.com>
2024-06-30 22:39:34 -0300

    Maximum Sub Array

---
+++ Problems.Library/Medium/MaximumSubArray/MaximumSubArraySolution.cs
@@ -0,0 +1,29 @@
+﻿namespace Problems.Library.Medium.MaximumSubArray
+{
+    /// <summary>
+    /// Solution for finding the subarray with the largest sum in a given integer array.
+    /// </summary>
+    public class MaximumSubArraySolution
+    {
+        /// <summary>
+        /// Finds the subarray with the largest sum and returns its sum.
+        /// </summary>
+        /// <param name="nums">The input array of integers.</param>
+        /// <returns>The sum of the subarray with the largest sum.</returns>
+        public int MaxSubArray(int[] nums)
+        {
+            if (nums == null || nums.Length == 0)
+            {
+                throw new ArgumentException("Array must contain at least one element");
+            }
+            int maxSum = nums[0];
+            int currentSum = maxSum;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                currentSum = Math.Max(nums[i], currentSum + nums[i]);
+                maxSum = Math.Max(currentSum, maxSum);
+            }
+            return maxSum;
+        }
+    }
+}
---
+++ Problems.Test/Medium/MaximumSubArrayTests.cs
@@ -0,0 +1,32 @@
+﻿using Problems.Library.Medium.MaximumSubArray;
+
+namespace Problems.Test.Medium
+{
+    public class MaximumSubArrayTests
+    {
+        public static IEnumerable<object[]> GetTestData()
+        {
+            yield return new object[] { new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 }, 6 };
+            yield return new object[] { new int[] { 1 }, 1 };
+            yield return new object[] { new int[] { 5, 4, -1, 7, 8 }, 23 };
+            yield return new object[] { new int[] { -1, -2, -3, -4 }, -1 };
+            yield return new object[] { new int[] { 0, -1, 2, -3, 4, -2, 2, 1 }, 5 };
+        }
+
+        [Theory]
+        [MemberData(nameof(GetTestData))]
+        public void MaxSubArray_Tests(int[] nums, int expected)
+        {
+            var solution = new MaximumSubArraySolution();
+            var result = solution.MaxSubArray(nums);
+            Assert.Equal(expected, result);
+        }
+        [Fact]
+        public void MaxSubArray_Tests_ShouldThrowArgumentException()
+        {
+            var solution = new MaximumSubArraySolution();
+            var nums = new int[] { };
+            Assert.Throws<ArgumentException>(() => solution.MaxSubArray(nums));
+        }
+    }
+}
